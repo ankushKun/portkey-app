ed0698c8c8c1db57b10d5e67aad6247fbcdc5c6e
ankushKun <ankush4singh@gmail.com>
2024-11-03 12:20:43 +0530

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Arweave from "arweave";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { generateMnemonic, getKeyFromMnemonic } from 'arweave-mnemonic-keys';
 
 const ar = Arweave.init({
   host: "arweave.net",
@@ -7,33 +8,33 @@ const ar = Arweave.init({
   protocol: "https",
 });
 
-async function scan() {
-  console.log("User clicked scan button");
+// async function scan() {
+//   console.log("User clicked scan button");
 
-  // alert(window.NDEFReader)
+//   // alert(window.NDEFReader)
 
-  try {
-    const ndef = new NDEFReader();
-    await ndef.scan();
-    console.log("> Scan started");
+//   try {
+//     const ndef = new NDEFReader();
+//     await ndef.scan();
+//     console.log("> Scan started");
 
-    ndef.addEventListener("readingerror", () => {
-      console.log("Argh! Cannot read data from the NFC tag. Try another one?");
-      alert("Argh! Cannot read data from the NFC tag. Try another one?");
-    });
+//     ndef.addEventListener("readingerror", () => {
+//       console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+//       alert("Argh! Cannot read data from the NFC tag. Try another one?");
+//     });
 
-    ndef.addEventListener("reading", (e) => {
-      console.log("NFC tag detected: ", e);
-      alert("NFC tag detected: " + JSON.stringify(e));
-      // console.log(serialNumber, message);
-      // console.log(`> Serial Number: ${serialNumber}`);
-      // console.log(`> Records: (${message.records.length})`);
-    });
-  } catch (error) {
-    console.log("Argh! " + error);
-  }
+//     ndef.addEventListener("reading", (e) => {
+//       console.log("NFC tag detected: ", e);
+//       alert("NFC tag detected: " + JSON.stringify(e));
+//       // console.log(serialNumber, message);
+//       // console.log(`> Serial Number: ${serialNumber}`);
+//       // console.log(`> Records: (${message.records.length})`);
+//     });
+//   } catch (error) {
+//     console.log("Argh! " + error);
+//   }
 
-}
+// }
 
 async function write(data: string) {
   console.log("User clicked write button");
@@ -64,29 +65,79 @@ async function write(data: string) {
 // }
 
 function App() {
-  const [w, setW] = useState<string>();
+  const [mnemonic, setMnemonic] = useState<string>("");
+  const [jwk, setJwk] = useState();
+  const [address, setAddress] = useState<string>("");
+
+  useEffect(() => {
+    async function load() {
+      if (!mnemonic) return;
+      if (mnemonic.split(" ").length !== 12) return;
+      const jwk = await getKeyFromMnemonic(mnemonic);
+      setJwk(jwk);
+    }
+    load();
+  }, [mnemonic])
+
+  useEffect(() => {
+    async function load() {
+      if (!jwk) return;
+      const w = await ar.wallets.jwkToAddress(jwk);
+      console.log(w);
+      setAddress(w);
+    }
+    load();
+  }, [jwk])
 
   async function handleWriteWallet() {
-    await write(JSON.stringify(w));
-    console.log(w);
+    await write(mnemonic);
+    console.log(mnemonic);
     alert("Wallet written to NFC tag");
   }
 
 
   async function genWallet() {
-    const w = await ar.wallets.generate();
-    setW(JSON.stringify(w));
+    console.log("gen wallet");
+    const m = await generateMnemonic();
+    setMnemonic(m);
+  }
+
+  async function readNfc() {
+    console.log("read wallet");
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    console.log("> Scan started");
+
+    ndef.addEventListener("readingerror", () => {
+      console.log("Argh! Cannot read data from the NFC tag. Try another one?");
+      alert("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
+
+    ndef.addEventListener("reading", async (e) => {
+      console.log("NFC tag detected: ", e);
+      const message = e.message;
+      console.log(message);
+      const decoder = new TextDecoder();
+      const text = decoder.decode(message.records[0].data);
+      console.log(text);
+      setMnemonic(text);
+    });
   }
 
   return (
     <>
       <div>NFC: {("NDEFReader" in window).toString()}</div>
-      <button onClick={scan}>scan</button><br /><br />
+      {/* <button onClick={scan}>scan</button><br /><br /> */}
 
-      <input type="text" value={w} onChange={(e) => setW(e.target.value)} />
-      <button onClick={handleWriteWallet}>write</button><br />
+      <div><pre>{address}</pre></div>
+      <input type="text" value={mnemonic} onChange={(e) => setMnemonic(e.target.value)} />
+      <button onClick={handleWriteWallet}>write {new TextEncoder().encode(mnemonic).length}</button><br />
 
       <button onClick={genWallet}>gen wallet</button>
+
+      <br />
+      <br />
+      <button onClick={readNfc}>read wallet</button>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/portkey-app/",
   plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: 'globalThis'
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  resolve: {
+    alias: {
+      // This Rollup alias is required to ensure that the Buffer polyfill is used
+      buffer: 'buffer'
+    }
+  }
 })
